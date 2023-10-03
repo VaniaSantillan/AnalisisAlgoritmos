f537b8d95fe6c95f9a6e0f5422c53d469af9ec8a
VaniaSantillan <142811154+VaniaSantillan@users.noreply.github.com>
2023-10-02 21:30:49 -0600

    Add files via upload

---
+++ AnalisisAlgoritmos/VocalesAcento/Vocales.js
@@ -0,0 +1,24 @@
+function VocalesAcento (f){
+
+    f="aá eÉ íÍ oO uÚ"
+    c=0
+
+    for (i=1; i<= f.lenght; i++){
+        if (f.lenght = "á" || "Á"){
+            c=c+1
+        }
+        if (f.lenght = "é" || "É"){
+            c=c+1
+        }
+        if (f.lenght = "í" || "Í"){
+            c=c+1
+        }
+        if (f.lenght = "ó" || "Ó"){
+            c=c+1
+        }
+        if (f.lenght = "ú" || "Ú"){
+            c=c+1
+        }
+    }
+    return c
+}
