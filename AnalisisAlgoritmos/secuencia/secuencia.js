b9dd8a4070ad986fb6f05065ef10fbe51129b578
VaniaSantillan <142811154+VaniaSantillan@users.noreply.github.com>
2023-10-19 11:10:18 -0600

    Delete AnalisisAlgoritmos/secuencia directory

--- AnalisisAlgoritmos/secuencia/secuencia.js
+++
@@ -1,8 +0,0 @@
-function imprimirSecuenciaDescendente(numero) {
-    if (numero >= 0) {
-        console.log(numero);
-        imprimirSecuenciaDescendente(numero - 1);
-    }
-}
-
-imprimirSecuenciaDescendente(15)
